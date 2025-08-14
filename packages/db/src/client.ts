e8ef1a830fcc2774fc11d1a04db51e8172f1dd71
Shaishav Pidadi <shaivpidadi@gmail.com>
2025-08-14 11:37:17 -0400

    fix setup

--- packages/db/src/client.ts
+++ packages/db/src/client.ts
@@ -1,10 +1,14 @@
-import { sql } from "@vercel/postgres";
+import { createClient } from "@vercel/postgres";
 import { drizzle } from "drizzle-orm/vercel-postgres";
 
 import * as schema from "./schema";
 
-export const db = drizzle({
-  client: sql,
-  schema,
-  casing: "snake_case",
-});
+export const db = drizzle(
+  createClient({
+    connectionString: process.env.POSTGRES_URL,
+  }),
+  {
+    schema,
+    casing: "snake_case",
+  }
+);
